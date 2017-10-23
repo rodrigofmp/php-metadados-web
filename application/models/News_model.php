2c7a69e7b4247609bd10ffb6904973e51cf8c933
Rodrigo Petters <rodrigofmp@gmail.com>
2017-10-23 20:00:22 -0200

    Form para cadastrar notícia

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -4,9 +4,9 @@ class News_model extends CI_Model {
         public function __construct()
         {
                 $this->load->database();
-				
+
         }
-		
+
 		public function get_news($slug = FALSE)
 		{
 				if ($slug === FALSE)
@@ -17,6 +17,21 @@ class News_model extends CI_Model {
 
 				$query = $this->db->get_where('news', array('slug' => $slug));
 				return $query->row_array();
-		}				
-		
+		}
+
+		public function set_news()
+		{
+				$this->load->helper('url');
+
+				$slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+				$data = array(
+					'title' => $this->input->post('title'),
+					'slug' => $slug,
+					'text' => $this->input->post('text')
+				);
+
+				return $this->db->insert('news', $data);
+		}
+
 }
